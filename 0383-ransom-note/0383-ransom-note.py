efc7c11754d32a727ab7b7dbf99ab1531a44b184
Tass-1 <priyanshujoshi10000@gmail.com>
2026-09-19 16:18:38 +0530

    0383-RANSOM-NOTE Stats: Time: 22 ms (56.79%), Space: 19.7 MB (9.12%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,15 +2,13 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         if len(ransomNote) > len(magazine):
             return False
-        ra = []
-        for i in range(len(ransomNote)):
-            ra.append(ransomNote[i])
+        ra = {}
+        for i in range(len(magazine)):
+            ra[magazine[i]] = ra.get(magazine[i] , 0) + 1
         j = 0
-
-        while j < len(magazine):
-            if magazine[j] in ra:
-                ra.remove(magazine[j])
+        while j < len(ransomNote):
+            if ransomNote[j] not in ra or (ra[ransomNote[j]] == 0):
+                return False
+            ra[ransomNote[j]] -= 1
             j += 1
-        if len(ra) == 0:
-            return True
-        return False
+        return True
