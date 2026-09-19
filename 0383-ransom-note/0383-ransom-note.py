e8deaa96a43f17f3a0b0a3e5881ec05f206366c3
Tass-1 <priyanshujoshi10000@gmail.com>
2026-09-19 16:20:27 +0530

    0383-RANSOM-NOTE Stats: Time: 22 ms (56.79%), Space: 19.8 MB (9.12%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,8 +3,8 @@ class Solution:
         if len(ransomNote) > len(magazine):
             return False
         ra = {}
-        for i in range(len(magazine)):
-            ra[magazine[i]] = ra.get(magazine[i] , 0) + 1
+        for i in magazine:
+            ra[i] = ra.get(i , 0) + 1
         j = 0
         while j < len(ransomNote):
             if ransomNote[j] not in ra or (ra[ransomNote[j]] == 0):
