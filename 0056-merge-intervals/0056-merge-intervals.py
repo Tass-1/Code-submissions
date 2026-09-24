98a7dd3f93cf2e8a81d6cb1daffc3e433f1c6487
Tass-1 <priyanshujoshi10000@gmail.com>
2026-09-24 15:15:00 +0530

    0056-MERGE-INTERVALS Stats: Time: 8 ms (43.69%), Space: 22.4 MB (87.36%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,12 +2,8 @@ class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
         k = -1
         res = []
-        if len(intervals) == 0:
-            return []
         intervals.sort()
-        print(intervals)
         for i in range(len(intervals)):
-
             curr = intervals[i]
             if curr[0] <= k and curr[1] >= k:
                 res[-1][1] = curr[1]
