b86b6b55b1e05d7645ffdacde7c3a74498abbe77
Tass-1 <priyanshujoshi10000@gmail.com>
2026-09-24 15:38:22 +0530

    0452-MINIMUM-NUMBER-OF-ARROWS-TO-BURST-BALLOONS Stats: Time: 68 ms (73.65%), Space: 53.7 MB (13.33%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,6 +1,7 @@
+from operator import itemgetter
 class Solution:
     def findMinArrowShots(self, points: list[list[int]]) -> int:
-        points.sort(key = lambda x:x[1])
+        points.sort(key = itemgetter(1))
         count = 1
         k = points[0][1]
         for start , end in points:
